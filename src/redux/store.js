dbbed80d6ea3b329de25691f03fd4121d99c7614
cq-hamza-kazmi <hamza.kazmi@cqtechnologies.com>
2020-04-16 19:57:02 +0500

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,7 @@
-import { createStore, applyMiddleware, compose} from 'redux'; 
-import logger from 'redux-logger';
+import { createStore } from 'redux';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
-
-const store = createStore(
-    rootReducer, 
-    compose(
-    applyMiddleware(...middlewares),window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(rootReducer);
 
 export default store;
