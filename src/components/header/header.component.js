91a90e367368f23ee1b0fde7cd8d0ed92dbd1af5
cq-hamza-kazmi <hamza.kazmi@cqtechnologies.com>
2020-04-16 19:37:18 +0500

    new changes

---
+++ src/components/header/header.component.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+import  './header.styles.scss';
+import { ReactComponent as Logo} from '../../assets/crown.svg';
+import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+const Header = ({ currentUser, hidden }) => (
+  <div className='header'>
+    <Link className='logo-container' to='/'>   
+       <Logo className='logo' />
+    </Link> 
+    <div className='options'>
+     <Link className='option' to='/shop'>
+         Shop
+     </Link>
+     <Link className='option' to='/shop'>
+         Contact
+     </Link>
+    
+    {currentUser ? <div className='option' onClick={() =>auth.signOut() }>Sign Out</div>
+    : <Link className='option' to='/signin'>Sign In</Link>  }
+    <CartIcon />
+    </div>
+  {   hidden ? null :  <CartDropdown/>}       
+  </div>
+);
+
+const mapStateToProps = ({user:{ currentUser }, cart:{hidden}}) => ({
+  currentUser,hidden
+})
+
+export default connect(mapStateToProps)(Header);
